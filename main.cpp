534b3ca53702899e2f8b0fa5efe8d62113316c31
develjacob <mcjacob61@gmail.com>
2018-02-14 15:41:36 -0900

    inital commit, catch2, main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+
+// Jacob McKenna
+// Paul Huskee
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::istringstream;
+using std::getline;
+
+
+TEST_CASE("add_string function -- single integer"){
+
+}
